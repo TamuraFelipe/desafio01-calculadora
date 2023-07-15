ceb3b56e63db36d8fc432d4bdaf870b99e83680a
Tamura <tamurafelipe@gmail.com>
2023-07-15 10:50:51 -0300

    feat: add a Input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {
+  InputContainer,
+} from './styles';
+
+export const Input = ({value}) => {
+  return (
+    <InputContainer>
+      <input disabled value={value}/>
+    </InputContainer>
+  )
+}
+
---
+++ src/components/Input/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 7.5rem;
+    background: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    input {
+        width: 100%;
+        height: 7.5rem;
+        background: #AAFFAA;
+        border: 0;
+        text-align: right;
+        font-size: 2.4rem;
+        padding-inline: 1.6rem;
+        direction: rtl;
+        unicode-bidi: bidi-override;
+    }
+`;
