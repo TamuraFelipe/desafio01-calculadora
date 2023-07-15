0e6d69efaa2d86ed3c81aabafad5fa09c7792325
Tamura <tamurafelipe@gmail.com>
2023-07-15 10:50:36 -0300

    feat: add a button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 2rem;
+    border: 1px solid #CDCDCD;
+    background: #00AAF0;
+    color: #FFF;
+    font-size: 2.4rem;
+    font-weight: 700;
+    flex: 1;
+    cursor: pointer;
+
+    &:hover {
+        opacity: .6;
+    }
+`;
