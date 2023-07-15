af65bd3ced282b923226fa50ae24633c1f740124
Tamura <tamurafelipe@gmail.com>
2023-07-15 14:12:58 -0300

    style: improves input styles

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,21 +3,26 @@ import styled from "styled-components";
 export const InputContainer = styled.div`
     width: 100%;
     height: 7.5rem;
-    background: #AAFFAA;
-
+    background: #fff;
+    border-radius: .5rem;
     display: flex;
     align-items: center;
     justify-content: flex-end;
 
+    grid-area: visor;
+
     input {
         width: 100%;
         height: 7.5rem;
-        background: #AAFFAA;
+        background: #fff;
         border: 0;
+        border-radius: .5rem;
         text-align: right;
         font-size: 2.4rem;
+        color: #232323;
         padding-inline: 1.6rem;
-        direction: rtl;
-        unicode-bidi: bidi-override;
+        
+        /* direction: rtl;
+        unicode-bidi: bidi-override; */
     }
 `;
