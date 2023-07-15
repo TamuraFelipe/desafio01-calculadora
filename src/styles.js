b356c5ebc2524a07bdb09de6be09169535264813
Tamura <tamurafelipe@gmail.com>
2023-07-15 10:50:17 -0300

    feat: add main styles

---
+++ src/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: #CACACA;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Content = styled.div`
+    width: 35rem;
+    background: #FFF;
+`;
+
+export const Row = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
